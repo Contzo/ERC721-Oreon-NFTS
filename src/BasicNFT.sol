51f83ad5af745a82cde445807e3bac67442a887e
Contzo <ilierazvan1497@outlook.com>
2025-05-14 15:58:41 +0300

    Added base CID

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
+    string baseURI = "QmYqgJgbiV1U1o3yHV41MPg7g6Nv8XjztqxH2hjNWU4Dch";
     uint256 private s_tokenCounter;
 
     constructor() ERC721("Oreon", "OREO") {
